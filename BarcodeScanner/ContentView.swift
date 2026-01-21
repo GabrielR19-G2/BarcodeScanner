e3f640ea3fcdc53972fe3c8a8d5704f1a815b81f
Gabriel Ramos <gabrielfcop8@gmail.com>
2026-01-21 07:05:01 -0700

    updated view

--- BarcodeScanner/ContentView.swift
+++ BarcodeScanner/ContentView.swift
@@ -10,10 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Barcode Scanner")
+                .frame(width: 370 , height: 70, alignment: .leading)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color(.label))
+                .scaledToFit()
+//                .padding(20)
+                
+            
+                Spacer()
+                .frame(height: 100)
+            
+            
+//            Text("framework.name")
+//                .font(.title2)
+//                .fontWeight(.semibold)
+//                .foregroundStyle(Color(.label))
+//                .scaledToFit() // ability to shrink
+//                .minimumScaleFactor(0.5)
+            
+            HStack{
+                Image(systemName: "barcode.viewfinder")
+                    .foregroundStyle(.black)
+                Text("Scanned Barcode: ")
+            }
+            Text("Not Yet Scanned")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color(.red))
+                .scaledToFit()
+                .padding(20)
         }
         .padding()
     }
